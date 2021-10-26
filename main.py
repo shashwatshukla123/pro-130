6972881692a04a4be7e30399e8ec5d31fe92c24c
shashwat shukla <72383260+shashwatshukla123@users.noreply.github.com>
2021-10-26 13:33:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import pandas as pd
+import csv
 
-df = pd.read_csv("final.csv")
-# Listing all columns.
-print(df.head(5))
+df = pd.read_csv('total_stars.csv')
+print(df)
 
-#Deleting the file
-del df["Luminosity"]
+print(df.columns)
 
-# Creating new file.
-df.to_csv('main.csv') 
+df.drop(['Unnamed: 0','Unnamed: 6', 'Star_name.1', 'Distance.1', 'Mass.1', 'Radius.1','Luminosity'],axis=1,inplace=True)
+print(df)
+
+final_data = df.dropna()
+print(final_data)
+
+final_data.reset_index(drop=True,inplace = True)
+print(final_data)
+
+final_data.to_csv('final_data.csv')
+
+final_data.info()
+
+final_data.describe()
+
+final_data.head(5)
+
+final_data.dtypes
